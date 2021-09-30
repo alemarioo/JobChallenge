12cee490db2ceec7640d8a6e664f1168c8bb6c26
mario ruano <marioruano25@gmail.com>
2021-09-30 01:09:26 -0600

    creacion de las rutas de la api

--- rutas/index.js
+++ rutas/index.js
@@ -1,6 +1,14 @@
+//const Crud = require('../Crud/crud_info');
 const {Router} = require('express');
+const express = require('express');
 const router = Router();
 
-router.post()
+router.post('/', express.json({type: '*/*'}), (req,res)=> {
+    
+})
+
+router.get('/distance', (req,res)=> {
+    console.log(req.query)
+})
 
 module.exports = router;
