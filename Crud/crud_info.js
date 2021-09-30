66beea1f1948ddfbf7b5e2df82a0d44ede51a9be
mario ruano <marioruano25@gmail.com>
2021-09-30 00:08:02 -0600

    Metodo y funcion de guardar en la base de datos MONGODB

---
+++ Crud/crud_info.js
@@ -0,0 +1,33 @@
+require('../mongo/connection')
+const LugarModel = require('../models/info')
+
+/**
+ * Metodo que Guarda la informacion en la base de datos
+ * @param {String} nombre Nombre Del lugar a registrar
+ * @param {Number} longitud Longitud del lugar
+ * @param {Number} latitud Latitud del lugar
+ */
+async function RegistarNuevoLugar(nombre, longitud, latitud) {
+
+    const LugarMongo = new LugarModel({
+        nombre: nombre,
+        longitud: longitud,
+        latitud: latitud
+    })
+
+    await LugarMongo.save((err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+/**
+ * Funcion que obtiene la informacion del lugar en la base de datos
+ * @param {String} nombre 
+ * @returns Retorna el json del lugar encontrado
+ */
+async function ObtenerLugar(nombre) {
+    const LugarEncontrado = await LugarModel.findOne({nombre: nombre})
+    return LugarEncontrado;
+}
